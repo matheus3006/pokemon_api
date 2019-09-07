52b01bab9dff5c25f8c4790bee3b9bb521f64eaf
matheus de souza <matheussouza18@live.com>
2019-09-07 13:08:23 -0300

    criando as paginas basicas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
-
-
+import React ,{useState, useEffect }from 'react';
+import pokemonList from './PokemonList';
+import axios from 'axios'
 function App() {
+  const const [pokemon, setPokemon] = useState([]);
+  useEffect(() => {
+    return () => {
+      axios.get("https://pokeapi.co/api/v2/pokemon").then(res=>{
+        setPokemon(res.data.results.map(p=> p.name)); 
+      });
+       
+    };
+  }, [])
+  
+
   return (
-   null
+   <pokemonList pokemon={pokemon}/>
    
   );
 }
---
+++ src/Pagination.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Pagination() {
+  return (
+    <div>
+
+    <div />
+  );
+}
