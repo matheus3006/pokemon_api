fb78cdea14d739c21a493602c98d7c9ea2d25de4
matheus de souza <matheussouza18@live.com>
2019-09-08 09:50:08 -0300

    criado primeira pag. E logica de funcionamento junto com um loading

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl,setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon" );
+  const [nextPageUrl,setNextPageUrl] = useState( );
+  const [prevPageUrl,setPrevPageUrl] = useState( );
+  const [loading,setLoading] = useState(true);
 
   useEffect( ()=> {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then(res=>{
+    setLoading(true);
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken:new axios.CancelToken(c=> cancel= c)
+    }).then(res=>{
+      setLoading(false);
+      setNextPageUrl(res.data.next);
+      setPrevPageUrl(res.data.previous);
      setPokemon(res.data.results.map(p=> p.name));
     });
-  }, []);
-   
+    return()=>  cancel();
+
+  }, [currentPageUrl]);
+  
+
+  if(loading){
+    return "Loading...";
+  } 
+
   return (
    <PokemonList pokemon={pokemon}/>
    
